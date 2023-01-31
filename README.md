715d58dd795f0cd7be1ab69d91651f1b98dcb546
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2023-01-31 12:32:09 -0300

    atualizando projeto com DOM

---
+++ README.md
@@ -0,0 +1,9 @@
+# Alura Books
+
+A Alura Books primeiramente foi um projeto de Flexbox e Grid.
+
+Agora, ele foi atualizado para ensinar a usar o API de CEP para a pessoa com a professora Mônica Hillman.
+
+No curso é falado de Promises, Then, Callback, API's, Async Await etc.
+
+Curso finalizado: 31/01/2022.
---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+
+        var bairro = document.getElementById('bairro')
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+
+        bairro.value = consultaCEPConvertida.bairro;
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
+        console.log(erro);
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
